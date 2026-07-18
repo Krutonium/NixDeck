40939e69211ab28f59477cbd574f2f32b8389e72
Krutonium <PFCKrutonium@gmail.com>
2026-07-17 22:17:05 -0400

    Disable Login Manager

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -4,5 +4,7 @@
     { ... }:
     {
       services.desktopManager.plasma6.enable = true;
+      services.displayManager.plasma-login-manager.enable = false;
+      services.displayManager.sddm.enable = false;
     };
 }
